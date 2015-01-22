7b04e17330915bc429d08f12f049e52c47f27462
taichi <taichi730@jf6.so-net.ne.jp>
2015-01-23 00:54:47 +0900

    子コンポーネントオブジェクト、アイテムオブジェクトの生成に対応

--- ruby/lib/register_generator/core/base/component_factory.rb
+++ ruby/lib/register_generator/core/base/component_factory.rb
@@ -1,20 +1,48 @@
 module RegisterGenerator::Base
   class ComponentFactory
+    def initialize
+      @item_factories = {}
+    end
+
     def create(*args)
       parent    = (@root_factory) ? nil : args.pop
-      component = create_component(parent, *args)
+      sources   = args
+
+      component = create_component(parent, *sources)
+
+      unless @item_factories.empty?
+        create_items(component, *sources)
+      end
+
+      if @child_factory
+        create_children(component, *sources)
+      end
+
+      component
     end
 
     def register_component(component)
       @target_component = component
     end
 
+    def register_item_factory(name, item_factory)
+      @item_factories[name] = item_factory
+    end
+
+    def register_child_factory(child_factory)
+      @child_factory  = child_factory
+    end
+
     def root_factory
       @root_factory = true
     end
 
-    def create_component(parent, *args)
+    def create_component(parent, *sources)
       @target_component.new(parent)
+    end
+
+    def create_child(component, *sources)
+      @child_factory.create(component, *sources)
     end
   end
 end
--- ruby/spec/core/base/component_factory_spec.rb
+++ ruby/spec/core/base/component_factory_spec.rb
@@ -39,6 +39,54 @@ module RegisterGenerator::Base
           expect(c.parent).to be parent
         end
       end
+
+      context "子コンポーネントファクトリが登録されているとき" do
+        let(:child_factory) do
+          create_factory
+        end
+
+        it "子コンポーネントを含むコンポーネントオブジェクトを生成する" do
+          f = create_factory do
+            def create_children(component, *args)
+              create_child(component, *args)
+            end
+          end
+          f.register_child_factory(child_factory)
+
+          c = f.create(parent)
+          expect(c.children).to match [kind_of(component_class)]
+        end
+      end
+
+      context "アイテムファクトリが登録されているとき" do
+        let(:item_class) do
+          Class.new(Item)
+        end
+
+        let(:item_name) do
+          :item
+        end
+
+        let(:item_factory) do
+          f = ItemFactory.new
+          f.register(item_name, item_class)
+          f
+        end
+
+        it "アイテムを含むコンポーネントオブジェクトを生成する" do
+          f = create_factory do
+            def create_items(component, *args)
+              @item_factories.each_value do|f|
+                f.create(component, *args)
+              end
+            end
+          end
+          f.register_item_factory(item_name, item_factory)
+
+          c = f.create(parent)
+          expect(c.items).to match [kind_of(item_class)]
+        end
+      end
     end
   end
 end
