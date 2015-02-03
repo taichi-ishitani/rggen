1ea16b58773d47201120204eabcb2b6c4749cd97
taichi <taichi730@jf6.so-net.ne.jp>
2015-02-03 18:26:50 +0900

    サンプルの実装を説明にあうように書き直し

--- ruby/spec/core/register_map/generic_map_spec.rb
+++ ruby/spec/core/register_map/generic_map_spec.rb
@@ -18,20 +18,24 @@ module RGen::RegisterMap
       ["foo", "bar"]
     end
 
+    def match_sheet(name)
+      be_kind_of(GenericMap::Sheet).and have_attributes(name: name)
+    end
+
     describe "#[]" do
       context "引数がシート名のとき" do
         it "シート名が'sheet'のシートオブジェクトを返す" do
           sheet = map[sheet_name]
-          expect(sheet).to be_kind_of(GenericMap::Sheet).and have_attributes(name: sheet_name)
+          expect(sheet).to match_sheet(sheet_name)
         end
 
         context "当該のシートがない場合" do
           it "新たにシートオブジェクトを追加する" do
-            sheets  = []
-            sheet_names.each do |name|
-              sheets  << map[name]
-            end
-            expect(map.sheets).to match sheets
+            expect {
+              sheet_names.each {|name| map[name]}
+            }.to change {map.sheets}.
+            from([]).
+            to([match_sheet(sheet_names[0]), match_sheet(sheet_names[1])])
           end
         end
 
@@ -42,7 +46,7 @@ module RGen::RegisterMap
               sheets[name]  = map[name]
             end
             sheets.each do |name, sheet|
-              expect(map[name]).to eq sheet
+              expect(map[name]).to eql sheet
             end
           end
         end
@@ -55,7 +59,7 @@ module RGen::RegisterMap
             sheets  << map[name]
           end
           sheets.each_with_index do |sheet, index|
-            expect(map[index]).to eq sheet
+            expect(map[index]).to eql sheet
           end
         end
       end
