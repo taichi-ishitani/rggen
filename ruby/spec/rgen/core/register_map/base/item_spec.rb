f13b1d37e32c1b4c75e47ee7581762897d993f05
taichi <taichi730@jf6.so-net.ne.jp>
2015-02-05 17:55:43 +0900

    specのディレクトリ構成をlibのディレクトリ構成に統一

--- ruby/spec/rgen/core/register_map/base/item_spec.rb
+++ ruby/spec/rgen/core/register_map/base/item_spec.rb
@@ -1,4 +1,4 @@
-require_relative  '../../../spec_helper'
+require_relative  '../../../../spec_helper'
 
 module RGen::RegisterMap::Base
   describe Item do
--- ruby/spec/rgen/core/register_map/generic_map/cell_spec.rb
+++ ruby/spec/rgen/core/register_map/generic_map/cell_spec.rb
@@ -1,4 +1,4 @@
-require_relative  '../../../spec_helper'
+require_relative  '../../../../spec_helper'
 
 class RGen::RegisterMap::GenericMap
   describe Cell do
