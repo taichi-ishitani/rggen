282c42fa94b3086fbba53ccd2bd2faa4db288245
taichi <taichi730@jf6.so-net.ne.jp>
2015-02-05 23:39:55 +0900

    RegisterMap::RegisterMap#registersを追加

--- ruby/lib/rgen/core/register_map/register_map.rb
+++ ruby/lib/rgen/core/register_map/register_map.rb
@@ -2,4 +2,8 @@ class RGen::RegisterMap::RegisterMap < RGen::InputBase::Component
   def register_blocks
     children
   end
+
+  def registers
+    register_blocks.flat_map(&:registers)
+  end
 end
--- ruby/spec/rgen/core/register_map/register_map_spec.rb
+++ ruby/spec/rgen/core/register_map/register_map_spec.rb
@@ -10,14 +10,29 @@ module RGen::RegisterMap
       2.times.map {RGen::RegisterMap::RegisterBlock::RegisterBlock.new(register_map)}
     end
 
-    describe "#register_blocks" do
-      before do
-        register_blocks.each {|block| register_map.append_child(block)}
+    let(:registers) do
+      2.times.flat_map do |i|
+        2.times.map {RGen::RegisterMap::Register::Register.new(register_blocks[i])}
       end
+    end
 
+    before do
+      register_blocks.each {|block| register_map.append_child(block)}
+      registers.each_with_index {|register, i| register_blocks[i/2].append_child(register)}
+    end
+
+    describe "#register_blocks" do
       it "配下のレジスタブロックオブジェクト一覧返す" do
         expect(register_map.register_blocks).to match([
           eql(register_blocks[0]), eql(register_blocks[1])
+        ])
+      end
+    end
+
+    describe "#registers" do
+      it "配下のレジスタオブジェクト一覧を返す" do
+        expect(register_map.registers).to match([
+          eql(registers[0]), eql(registers[1]), eql(registers[2]), eql(registers[3])
         ])
       end
     end
