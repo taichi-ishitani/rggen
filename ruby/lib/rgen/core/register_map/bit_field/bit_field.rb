da63ef007c63d432d9b9723aba4d2955d4b80617
taichi <taichi730@jf6.so-net.ne.jp>
2015-02-09 23:33:51 +0900

    RegisterMap::BitField::BitFieldのスペックを追加

--- ruby/lib/rgen/core/register_map/bit_field/bit_field.rb
+++ ruby/lib/rgen/core/register_map/bit_field/bit_field.rb
@@ -1,2 +1,13 @@
 class RGen::RegisterMap::BitField::BitField < RGen::InputBase::Component
+  def register_map
+    register.register_map
+  end
+
+  def register_block
+    register.register_block
+  end
+
+  def register
+    parent
+  end
 end
---
+++ ruby/spec/rgen/core/register_map/bit_field/bit_field_spec.rb
@@ -0,0 +1,39 @@
+require_relative  '../../../../spec_helper'
+
+module RGen::RegisterMap::BitField
+  describe BitField do
+    let(:register_map) do
+      RGen::RegisterMap::RegisterMap.new
+    end
+
+    let(:register_block) do
+      RGen::RegisterMap::RegisterBlock::RegisterBlock.new(register_map)
+    end
+
+    let(:register) do
+      RGen::RegisterMap::Register::Register.new(register_block)
+    end
+
+    let(:bit_field) do
+      BitField.new(register)
+    end
+
+    describe "#register_map" do
+      it "属するレジスタマップオブジェクトを返す" do
+        expect(bit_field.register_map).to eql register_map
+      end
+    end
+
+    describe "#register_block" do
+      it "属するレジスタブロックオブジェクトを返す" do
+        expect(bit_field.register_block).to eql register_block
+      end
+    end
+
+    describe "#register" do
+      it "属するレジスタオブジェクトを返す" do
+        expect(bit_field.register).to eql register
+      end
+    end
+  end
+end
