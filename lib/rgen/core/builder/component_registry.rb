0d5166cfa237bc2bfd3f9cd9ced7ef901a7f9ba8
taichi <taichi730@jf6.so-net.ne.jp>
2015-02-26 12:57:03 +0900

    typoを修正

--- lib/rgen/core/builder/component_registry.rb
+++ lib/rgen/core/builder/component_registry.rb
@@ -6,12 +6,12 @@ module RGen::Builder
       @entries  = []
     end
 
-    def register_component(assosiated_category = nil, &body)
+    def register_component(associated_category = nil, &body)
       entry = ComponentEntry.new
       entry.instance_exec(&body)
 
       @builder.categories.each do |name, category|
-        if assosiated_category.nil? || name == assosiated_category
+        if associated_category.nil? || name == associated_category
           category.append_item_registry(@name, entry.item_registry)
         end
       end if entry.item_registry
