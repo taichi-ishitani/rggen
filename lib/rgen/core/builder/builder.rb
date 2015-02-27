a9d563b8a2785e0103a1936d986699e47fd17dc9
taichi <taichi730@jf6.so-net.ne.jp>
2015-02-27 12:42:46 +0900

    Builder::Builder#component_registryと#build_factoryを追加

--- lib/rgen/core/builder/builder.rb
+++ lib/rgen/core/builder/builder.rb
@@ -8,11 +8,21 @@ module RGen::Builder
     ].freeze
 
     def initialize
+      @registries = {}
       @categories = INITIAL_CATEGORIES.each_with_object({}) do |category, hash|
         hash[category]  = Category.new
       end
     end
 
     attr_reader :categories
+
+    def component_registry(registry_name, &body)
+      @registries[registry_name]  ||= ComponentRegistry.new(self, registry_name)
+      @registries[registry_name].instance_exec(&body)
+    end
+
+    def build_factory(registry_name)
+      @registries[registry_name].build_factory
+    end
   end
 end
--- spec/rgen/core/builder/builder_spec.rb
+++ spec/rgen/core/builder/builder_spec.rb
@@ -6,6 +6,10 @@ module RGen::Builder
       Builder.new
     end
 
+    let(:registries) do
+      builder.instance_variable_get(:@registries)
+    end
+
     it "4種類のカテゴリを持つ" do
       expect(builder.categories).to match(
         global:         be_kind_of(Category),
@@ -13,6 +17,49 @@ module RGen::Builder
         register:       be_kind_of(Category),
         bit_field:      be_kind_of(Category)
       )
+    end
+
+    describe "#component_registry" do
+      it "コンポーネントエントリを生成し、引数で与えられた名前で登録する" do
+        registry  = nil
+        builder.component_registry(:register_map) do
+          register_component do
+            component_class   RGen::RegisterMap::RegisterMap
+            component_factory RGen::RegisterMap::Factory
+          end
+          register_component(:register_block) do
+            component_class   RGen::RegisterMap::RegisterBlock::RegisterBlock
+            component_factory RGen::RegisterMap::RegisterBlock::Factory
+            item_base         RGen::RegisterMap::RegisterBlock::Item
+            item_factory      RGen::RegisterMap::RegisterBlock::ItemFactory
+          end
+          registry  = self
+        end
+
+        expect(registries[:register_map]).to eql registry
+      end
+    end
+
+    describe "#build_factory" do
+      before do
+        builder.component_registry(:register_map) do
+          register_component do
+            component_class   RGen::RegisterMap::RegisterMap
+            component_factory RGen::RegisterMap::Factory
+          end
+          register_component(:register_block) do
+            component_class   RGen::RegisterMap::RegisterBlock::RegisterBlock
+            component_factory RGen::RegisterMap::RegisterBlock::Factory
+            item_base         RGen::RegisterMap::RegisterBlock::Item
+            item_factory      RGen::RegisterMap::RegisterBlock::ItemFactory
+          end
+        end
+      end
+
+      it "引数で与えられた名前のコンポーネントレジストリの#build_factoryを呼び出して、ファクトリの生成を行う" do
+        expect(registries[:register_map]).to receive(:build_factory).and_call_original
+        builder.build_factory(:register_map)
+      end
     end
   end
 end
