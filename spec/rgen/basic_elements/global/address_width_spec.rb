efb02e3a8e7d6765dab279aac08c641bff8a8bb8
taichi <taichi730@jf6.so-net.ne.jp>
2015-03-03 11:50:03 +0900

    サンプルの期待値を修正

--- spec/rgen/basic_elements/global/address_width_spec.rb
+++ spec/rgen/basic_elements/global/address_width_spec.rb
@@ -12,10 +12,6 @@ describe 'address_width/configuration' do
     clear_enabled_items
   end
 
-  let(:configuration) do
-    @factory.create(configuration_file)
-  end
-
   describe "#address_width" do
     context "ロード結果のHashに:address_widthが含まれないとき" do
       before do
@@ -35,11 +31,9 @@ describe 'address_width/configuration' do
 
       it "変換結果を返す" do
         load_data.each do |data|
-          w = Integer(data)
-          ConfigurationDummyLoader.load_data({address_width: w})
-
+          ConfigurationDummyLoader.load_data({address_width: data})
           c = @factory.create(configuration_file)
-          expect(c).to match_address_width(w)
+          expect(c).to match_address_width(Integer(data))
         end
       end
     end
