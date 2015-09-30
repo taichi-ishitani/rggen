9dd3e53c1fecc30509b88ee1cfb8fa56dd482a63
taichi <taichi730@jf6.so-net.ne.jp>
2015-10-01 02:07:38 +0900

    Forwardable#def_class_delegator, #def_class_delegatorsを復活

--- lib/rgen/core/core_extensions/forwardable.rb
+++ lib/rgen/core/core_extensions/forwardable.rb
@@ -1,4 +1,15 @@
 require 'forwardable'
+
+module Forwardable
+  def def_class_delegator(method, ali = method)
+    def_delegator("self.class", method, ali)
+  end
+
+  def def_class_delegators(*methods)
+    def_instance_delegators("self.class", *methods)
+  end
+end
+
 module SingleForwardable
   def def_object_delegator(target, method, ali = method)
     define_singleton_method(ali) do |*args, &block|
