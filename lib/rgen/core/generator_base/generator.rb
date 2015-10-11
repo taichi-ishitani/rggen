5116fdb1926e673e0a0a5125a57e3d7939b769ad
taichi <taichi730@jf6.so-net.ne.jp>
2015-10-11 19:43:21 +0900

    rubucopの実行結果を反映

--- lib/rgen/core/generator_base/generator.rb
+++ lib/rgen/core/generator_base/generator.rb
@@ -3,19 +3,25 @@ module RGen::GeneratorBase
     def generate_code(kind, mode, buffer)
       case mode
       when :top_down
-        items.each do |item|
-          item.generate_code(kind, buffer)
-        end
-        children.each do |child|
-          child.generate_code(kind, mode, buffer)
-        end
+        generate_item_code(kind, buffer)
+        generate_child_code(kind, mode, buffer)
       when :bottom_up
-        children.each do |child|
-          child.generate_code(kind, mode, buffer)
-        end
-        items.each do |item|
-          item.generate_code(kind, buffer)
-        end
+        generate_child_code(kind, mode, buffer)
+        generate_item_code(kind, buffer)
+      end
+    end
+
+    private
+
+    def generate_child_code(kind, mode, buffer)
+      children.each do |child|
+        child.generate_code(kind, mode, buffer)
+      end
+    end
+
+    def generate_item_code(kind, buffer)
+      items.each do |item|
+        item.generate_code(kind, buffer)
       end
     end
   end
