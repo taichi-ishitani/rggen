1332b74e3513258fc354f78d3a45ad1061351d09
taichi <taichi730@jf6.so-net.ne.jp>
2015-10-14 22:00:50 +0900

    未使用のexampleを削除

--- spec/rgen/core/base/component_spec.rb
+++ spec/rgen/core/base/component_spec.rb
@@ -2,29 +2,12 @@ require_relative  '../../../spec_helper'
 
 module RGen::Base
   describe Component do
-    describe "#parent" do
-      it "親コンポーネントを返す" do
-        parent  = Component.new
-        child   = Component.new(parent)
-        expect(child.parent).to eql parent
-      end
-    end
-
     describe "#add_item" do
       it "アイテムコンポーネントを、#itemsの末尾に追加する" do
         component = Component.new
         item      = Object.new
         component.add_item(item)
         expect(component.items.last).to eql item
-      end
-    end
-
-    describe "#add_child" do
-      it "子コンポーネントを、#childrenの末尾に追加する" do
-        parent  = Component.new
-        child   = Object.new
-        parent.add_child(child)
-        expect(parent.children.last).to eql child
       end
     end
   end
