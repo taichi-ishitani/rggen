ccf8495acce0a85db875abab896761b930892fe8
taichi <taichi730@jf6.so-net.ne.jp>
2015-12-13 06:01:19 +0900

    オプションに--configurationを追加

--- lib/rgen/generator.rb
+++ lib/rgen/generator.rb
@@ -1,22 +1,56 @@
 module RGen
   class Generator
+    class << self
+      Option = Struct.new(:short, :long, :description)
+
+      def options
+        @options  ||= {}
+      end
+
+      private
+
+      def add_option(option_name, &body)
+        options[option_name]  = Option.new
+        body.call(options[option_name])
+      end
+    end
+
+    add_option :configuration do |option|
+      option.short  = '-c'
+      option.long   = '--configuration FILE'
+    end
+
     def run(argv)
-      parse_options!(argv)
+      options       = parse_options(argv)
+      configuration = load_configuration(options[:configuration])
     end
 
     private
 
-    def parse_options!(argv)
+    def parse_options(argv)
       options = {}
       option_parser(options).parse!(argv)
       options
     end
 
     def option_parser(options)
-      parser              = OptionParser.new
-      parser.version      = RGen::VERSION
-      parser.program_name = 'rgen'
-      parser
+      OptionParser.new do |parser|
+        parser.version      = RGen::VERSION
+        parser.program_name = 'rgen'
+        add_option_switches(parser, options)
+      end
+    end
+
+    def add_option_switches(parser, options)
+      self.class.options.each do |name, option|
+        parser.on(*option.values.compact) do |v|
+          options[name] = v
+        end
+      end
+    end
+
+    def load_configuration(file)
+      RGen.builder.build_factory(:configuration).create(file)
     end
   end
 end
--- spec/rgen/generator_spec.rb
+++ spec/rgen/generator_spec.rb
@@ -28,6 +28,48 @@ module RGen
           expect($stdout.string).to eq("rgen #{RGen::VERSION}\n" * 2)
         end
       end
+
+      describe "-c/--configuration" do
+        let(:configuration_factory) do
+          f = RGen.builder.build_factory(:configuration)
+          allow(RGen.builder).to receive(:build_factory).and_wrap_original do |m, *args|
+            if args[0] == :configuration
+              f
+            else
+              m.call(*args)
+            end
+          end
+          f
+        end
+
+        context "コンフィグレーションファイルの指定が無い場合" do
+          before do
+            expect(configuration_factory).to receive(:create).with(nil).and_call_original
+          end
+
+          it "デフォルト値でコンフィグレーションを生成する" do
+            expect {
+              generator.run([])
+            }.not_to raise_error
+          end
+        end
+
+        context "コンフィグレーションファイルの指定が無い場合" do
+          before do
+            expect(configuration_factory).to receive(:create).with('foo.yaml').and_call_original
+            expect(configuration_factory).to receive(:create).with('foo.json').and_call_original
+          end
+
+          it "指定したファイルからコンフィグレーションを生成する" do
+            expect {
+              generator.run(["-c", "foo.yaml"])
+            }.to raise_error Errno::ENOENT
+            expect {
+              generator.run(["--configuration", "foo.json"])
+            }.to raise_error Errno::ENOENT
+          end
+        end
+      end
     end
   end
 end
