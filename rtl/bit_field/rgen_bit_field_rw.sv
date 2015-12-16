3bde84a02ea61d907825803977f7bae8f594f842
taichi <taichi730@jf6.so-net.ne.jp>
2015-12-17 02:55:31 +0900

    インデントを修正

--- rtl/bit_field/rgen_bit_field_rw.sv
+++ rtl/bit_field/rgen_bit_field_rw.sv
@@ -14,7 +14,7 @@ module rgen_bit_field_rw #(
   logic [WIDTH-1:0] write_data;
   logic [WIDTH-1:0] value;
 
-  assign  o_value = value;
+  assign  o_value     = value;
   assign  write_data  = (i_write_data & ( i_write_mask))
                       | (value        & (~i_write_mask));
   always_ff @(posedge clk or negedge rst_n) begin
