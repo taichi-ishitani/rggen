2f53a3e51f2ac32e8cc8646c69a8558983adebf2
taichi <taichi730@jf6.so-net.ne.jp>
2015-12-22 16:50:14 +0900

    - ポート名のタイポを修正 - 読み出しデータ、ステータスを保持するタイミングを変更 - statusの初期値のビット幅間違いを修正

--- rtl/register_block/rgen_response_mux.sv
+++ rtl/register_block/rgen_response_mux.sv
@@ -8,18 +8,20 @@ module rgen_response_mux #(
   output                        o_response_ready,
   output  [DATA_WIDTH-1:0]      o_read_data,
   output  [1:0]                 o_status,
-  input   [TOTAL_REGISTERS-1:0] i_regiter_select,
+  input   [TOTAL_REGISTERS-1:0] i_register_select,
   input   [DATA_WIDTH-1:0]      i_register_read_data[TOTAL_REGISTERS]
 );
   //  Response ready
+  logic response_valid;
   logic response_ready;
 
   assign  o_response_ready  = response_ready;
+  assign  response_valid    = (i_command_valid && (!response_ready)) ? 1'b1 : 1'b0;
   always_ff @(posedge clk or negedge rst_n) begin
     if (!rst_n) begin
       response_ready  <= 1'b0;
     end
-    else if (i_command_valid && (!response_ready)) begin
+    else if (response_valid) begin
       response_ready  <= 1'b1;
     end
     else begin
@@ -33,17 +35,17 @@ module rgen_response_mux #(
   logic [1:0] status;
 
   assign  o_status      = status;
-  assign  slave_error   = (TOTAL_REGISTERS == 1) ? ~i_regiter_select[0] : ~|i_regiter_select;
+  assign  slave_error   = (TOTAL_REGISTERS == 1) ? ~i_register_select[0] : ~|i_register_select;
   assign  exokay        = 1'b0;
   always_ff @(posedge clk or negedge rst_n) begin
     if (!rst_n) begin
-      status  <= 3'b000;
+      status  <= 2'b00;
     end
-    else if (i_command_valid) begin
+    else if (response_valid) begin
       status  <= {exokay, slave_error};
     end
     else begin
-      status  <= 3'b000;
+      status  <= 2'b00;
     end
   end
 
@@ -56,7 +58,7 @@ module rgen_response_mux #(
     if (!rst_n)  begin
       read_data <= {DATA_WIDTH{1'b0}};
     end
-    else if (i_command_valid) begin
+    else if (response_valid) begin
       read_data <= selected_data;
     end
     else begin
@@ -69,7 +71,7 @@ module rgen_response_mux #(
       logic [TOTAL_REGISTERS-1:0] temp;
       assign  selected_data[i]  = |temp;
       for (genvar j = 0;j < TOTAL_REGISTERS;j++) begin
-        assign  temp[j] = i_regiter_select[j] & i_register_read_data[j][i];
+        assign  temp[j] = i_register_select[j] & i_register_read_data[j][i];
       end
     end
   end
