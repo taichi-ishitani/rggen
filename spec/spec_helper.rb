f09ecdfd27e554b7c94854db4b7b0121af01a358
taichi <taichi730@jf6.so-net.ne.jp>
2015-12-26 21:20:30 +0900

    Code Climateでのカバレッジに対応

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,3 +3,8 @@ require_relative '../lib/rgen'
 require_relative  'support/helper_methods'
 require_relative  'support/matchers'
 require_relative  'support/config'
+
+if ENV['TRAVIS']
+  require "codeclimate-test-reporter"
+  CodeClimate::TestReporter.start
+end
