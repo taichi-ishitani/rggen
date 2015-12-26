c98f217c26c72640ba2a730c1adbc4e4fabdf169
taichi <taichi730@jf6.so-net.ne.jp>
2015-12-26 21:40:37 +0900

    カバレッジを有効にする位置を先頭に変更

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,10 +1,11 @@
+if ENV['TRAVIS']
+  require "codeclimate-test-reporter"
+  CodeClimate::TestReporter.start
+end
+
 require_relative '../lib/rgen'
 
 require_relative  'support/helper_methods'
 require_relative  'support/matchers'
 require_relative  'support/config'
 
-if ENV['TRAVIS']
-  require "codeclimate-test-reporter"
-  CodeClimate::TestReporter.start
-end
