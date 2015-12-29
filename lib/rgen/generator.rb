31b0a2ca46018143391a7c63a68c14bfa711489a
taichi <taichi730@jf6.so-net.ne.jp>
2015-12-29 16:36:49 +0900

    オプションの説明を追加

--- lib/rgen/generator.rb
+++ lib/rgen/generator.rb
@@ -1,34 +1,97 @@
 module RGen
   class Generator
-    class << self
-      Option = Struct.new(:short, :long, :default, :description)
+    class Option
+      def initialize(kind)
+        @kind = kind
+      end
 
+      attr_accessor :short
+      attr_accessor :long
+      attr_writer   :default
+      attr_writer   :description
+      attr_writer   :body
+
+      def on(parser, options)
+        options[@kind]  = @default if @default
+        parser.on(*args) do |value|
+          parser.instance_exec(value, options, @kind, &body)
+        end
+      end
+
+      def args
+        [@short, @long, description].compact
+      end
+
+      def description
+        return nil unless @description || @default
+        d = ''
+        d << @description.to_s
+        d << "(default: #{@default})" if @default
+        d
+      end
+
+      def body
+        @body || default_body
+      end
+
+      def default_body
+        proc do |value, options, kind|
+          options[kind] = value
+        end
+      end
+    end
+
+    class << self
       def options
-        @options  ||= {}
+        @options  ||= []
       end
 
       private
 
-      def add_option(option_name, &body)
-        options[option_name]  = Option.new
-        body.call(options[option_name])
+      def add_option(kind, &body)
+        options << Option.new(kind)
+        body.call(options.last)
       end
     end
 
     add_option :setup do |option|
       option.long     = '--setup FILE'
       option.default  = File.join(RGEN_HOME, 'setup', 'default.rb')
+      option.description  = 'Specify a setup file to set up RGen tool'
     end
 
     add_option :configuration do |option|
-      option.short  = '-c'
-      option.long   = '--configuration FILE'
+      option.short        = '-c'
+      option.long         = '--configuration FILE'
+      option.description  = 'Specify a configuration file ' \
+                            'for generated source code'
     end
 
     add_option :output do |option|
-      option.short    = '-o'
-      option.long     = '--output DIR'
-      option.default  = './'
+      option.short        = '-o'
+      option.long         = '--output DIR'
+      option.default      = './'
+      option.description  = 'Specify output directory'
+    end
+
+    add_option :version do |option|
+      option.short        = '-v'
+      option.long         = '--version'
+      option.description  = 'Display the version'
+      option.body         = proc do
+        puts ver
+        exit
+      end
+    end
+
+    add_option :help do |option|
+      option.short        = '-h'
+      option.long         = '--help'
+      option.description  = 'Display this message'
+      option.body         = proc do
+        puts help
+        exit
+      end
     end
 
     def run(argv)
@@ -51,16 +114,14 @@ module RGen
       OptionParser.new do |parser|
         parser.version      = RGen::VERSION
         parser.program_name = 'rgen'
+        parser.banner       = 'Usage: rgen [options] REGISTER_MAP'
         add_option_switches(parser, options)
       end
     end
 
     def add_option_switches(parser, options)
-      self.class.options.each do |name, option|
-        options[name] = option.default
-        parser.on(*option.values.compact) do |v|
-          options[name] = v
-        end
+      self.class.options.each do |option|
+        option.on(parser, options)
       end
     end
 
