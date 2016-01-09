b870d7aee43abb0e59affe9c119767f6caa4a039
taichi <taichi730@jf6.so-net.ne.jp>
2016-01-10 00:21:29 +0900

    正規表現の制度を向上

--- lib/rgen/input_base/regexp_patterns.rb
+++ lib/rgen/input_base/regexp_patterns.rb
@@ -1,15 +1,15 @@
 module RGen
   module InputBase
     module RegxpPatterns
-      BIN_REGEXP  = /0b(?:[01]|(?:[01][01_]*[01]))/i.freeze
-      OCT_REGEXP  = /0o(?:[0-7]|(?:[0-7][0-7_]*[0-7]))/i.freeze
-      DEC_REGEXP  = /\d|[1-9][0-9_]*[0-9]/.freeze
-      HEX_REGEXP  = /0x(?:\h|\h[\h_]*\h)/i
+      BIN_REGEXP  = /0b(?:[01][01_]*)?[01]/i.freeze
+      OCT_REGEXP  = /0o(?:[0-7][0-7_]*)?[0-7]/i.freeze
+      DEC_REGEXP  = /(?:[1-9][\d_]*)?\d/.freeze
+      HEX_REGEXP  = /0x(?:\h[\h_]*)?\h/i
 
       UNSIGNED_NUMBER_REGEXP  = (
-        BIN_REGEXP | OCT_REGEXP | DEC_REGEXP | HEX_REGEXP
+        /\b/ + (BIN_REGEXP | OCT_REGEXP | DEC_REGEXP | HEX_REGEXP) + /\b/
       ).freeze
-      VARIABLE_NAME_REGEXP    = /[a-z_][a-z0-9_]*/i.freeze
+      VARIABLE_NAME_REGEXP    = /\b[a-z_][a-z0-9_]*\b/i.freeze
       BLANK_REGEXP            = /[ \t]*/.freeze
 
       def self.included(klass)
