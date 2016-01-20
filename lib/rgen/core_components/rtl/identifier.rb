252dd75e16c3727afc7531b60501219fd7bfa2c2
taichi <taichi730@jf6.so-net.ne.jp>
2016-01-20 12:13:50 +0900

    Rtl::Identifier#[]にインデックスを配列で渡したときの動作を追加

--- lib/rgen/core_components/rtl/identifier.rb
+++ lib/rgen/core_components/rtl/identifier.rb
@@ -9,13 +9,17 @@ module RGen
         @name
       end
 
-      def [](msb, lsb = msb)
-        if msb.nil?
+      def [](indexes_or_msb, lsb = indexes_or_msb)
+        if indexes_or_msb.nil?
           self
-        elsif msb == lsb
-          Identifier.new("#{@name}[#{msb}]")
+        elsif indexes_or_msb.is_a?(Array)
+          indexes_or_msb.inject(self) do |identifer, index|
+            identifer[index]
+          end
+        elsif indexes_or_msb == lsb
+          Identifier.new("#{@name}[#{indexes_or_msb}]")
         else
-          Identifier.new("#{@name}[#{msb}:#{lsb}]")
+          Identifier.new("#{@name}[#{indexes_or_msb}:#{lsb}]")
         end
       end
     end
--- spec/rgen/core_components/rtl/identifier_spec.rb
+++ spec/rgen/core_components/rtl/identifier_spec.rb
@@ -10,6 +10,10 @@ module RGen::Rtl
       2
     end
 
+    let(:indexes) do
+      [3, 4, 5]
+    end
+
     let(:msb) do
       1
     end
@@ -41,6 +45,12 @@ module RGen::Rtl
         end
       end
 
+      context "#[]で配列が与えられた場合" do
+        it "連続したビット選択込みの変数名を返す" do
+          expect(identifier[indexes].to_s).to eq "#{name}#{indexes.map { |i| "[#{i}]" }.join}"
+        end
+      end
+
       context "#[]でパート選択がされた場合" do
         it "パート選択込みの変数名を返す" do
           expect(identifier[msb, lsb].to_s).to eq "#{name}[#{msb}:#{lsb}]"
@@ -49,7 +59,7 @@ module RGen::Rtl
 
       context "複数回、パート選択やビット選択がされた場合" do
         it "全ての選択操作を含む変数名を返す" do
-          expect(identifier[msb, lsb][lsb].to_s).to eq "#{name}[#{msb}:#{lsb}][#{lsb}]"
+          expect(identifier[indexes][msb, lsb][lsb].to_s).to eq "#{name}#{indexes.map { |i| "[#{i}]" }.join}[#{msb}:#{lsb}][#{lsb}]"
         end
       end
     end
