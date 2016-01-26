beaa56bd915d6d8f6a9c964779e1d3a6d5a43ea7
taichi <taichi730@jf6.so-net.ne.jp>
2016-01-27 01:30:05 +0900

    不要な括弧を削除

--- lib/rgen/core_components/register_map/setup.rb
+++ lib/rgen/core_components/register_map/setup.rb
@@ -1,26 +1,26 @@
 module RGen
   module RegisterMap
-    input_component_store(:register_map) do
+    input_component_store :register_map do
       entry do
         component_class   Component
         component_factory RegisterMapFactory
       end
 
-      entry(:register_block) do
+      entry :register_block do
         component_class   Component
         component_factory RegisterBlockFactory
         item_base         Item
         item_factory      ItemFactory
       end
 
-      entry(:register) do
+      entry :register do
         component_class   Component
         component_factory RegisterFactory
         item_base         Item
         item_factory      ItemFactory
       end
 
-      entry(:bit_field) do
+      entry :bit_field do
         component_class   Component
         component_factory BitFieldFactory
         item_base         Item
--- lib/rgen/core_components/rtl/setup.rb
+++ lib/rgen/core_components/rtl/setup.rb
@@ -1,26 +1,26 @@
 module RGen
   module Rtl
-    output_component_store(:rtl) do
+    output_component_store :rtl do
       entry do
         component_class   OutputBase::Component
         component_factory OutputBase::ComponentFactory
       end
 
-      entry(:register_block) do
+      entry :register_block do
         component_class   Component
         component_factory OutputBase::ComponentFactory
         item_base         Item
         item_factory      OutputBase::ItemFactory
       end
 
-      entry(:register) do
+      entry :register do
         component_class   Component
         component_factory OutputBase::ComponentFactory
         item_base         Item
         item_factory      OutputBase::ItemFactory
       end
 
-      entry(:bit_field) do
+      entry :bit_field do
         component_class   Component
         component_factory OutputBase::ComponentFactory
         item_base         Item
