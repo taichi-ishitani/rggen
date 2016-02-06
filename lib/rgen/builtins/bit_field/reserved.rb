c282b2d65f8240cdcf0e84da1fc6bd48392a9340
taichi <taichi730@jf6.so-net.ne.jp>
2016-02-07 00:30:49 +0900

    reservedのアクセス権をROに設定

--- lib/rgen/builtins/bit_field/reserved.rb
+++ lib/rgen/builtins/bit_field/reserved.rb
@@ -2,4 +2,8 @@ list_item :bit_field, :type, :reserved  do
   register_map do
     reserved
   end
+
+  ral do
+    access :ro
+  end
 end
--- spec/rgen/builtins/bit_field/reserved_spec.rb
+++ spec/rgen/builtins/bit_field/reserved_spec.rb
@@ -3,16 +3,21 @@ require_relative '../spec_helper'
 describe 'bit_fields/type/reserved' do
   include_context 'bit field type common'
   include_context 'configuration common'
+  include_context 'ral common'
 
   before(:all) do
-    RGen.enable(:global, :data_width)
-    RGen.enable(:register_block, :name)
-    RGen.enable(:register, :name)
-    RGen.enable(:bit_field, [:name, :bit_assignment, :type, :reference])
-    RGen.enable(:bit_field, :type, [:reserved, :rw])
+    enable :register_block, :name
+    enable :register, :name
+    enable :bit_field, [:name, :bit_assignment, :type, :reference]
+    enable :bit_field, :type, [:reserved, :rw]
 
-    @configuration_factory  = build_configuration_factory
     @factory                = build_register_map_factory
+  end
+
+  before(:all) do
+    ConfigurationDummyLoader.load_data({})
+    enable :global, :data_width
+    @configuration  = build_configuration_factory.create(configuration_file)
   end
 
   after(:all) do
@@ -20,46 +25,66 @@ describe 'bit_fields/type/reserved' do
   end
 
   let(:configuration) do
-    ConfigurationDummyLoader.load_data({})
-    @configuration_factory.create(configuration_file)
+    @configuration
   end
 
-  describe "#type" do
-    it ":reservedを返す" do
+  describe "register_map" do
+    describe "#type" do
+      it ":reservedを返す" do
+        bit_fields  = build_bit_fields([
+          [nil, "register_0", "bit_field_0_0", "[0]", "reserved", nil]
+        ])
+        expect(bit_fields[0].type).to be :reserved
+      end
+    end
+
+    it "アクセス属性はreserved" do
       bit_fields  = build_bit_fields([
         [nil, "register_0", "bit_field_0_0", "[0]", "reserved", nil]
       ])
-      expect(bit_fields[0].type).to be :reserved
+      expect(bit_fields[0]).to match_access(:reserved)
+    end
+
+    it "任意のビット幅を持つビットフィールドで使用できる" do
+      expect {
+        build_bit_fields([
+          [nil, "register_0", "bit_field_0_0", "[0]"   , "reserved", nil],
+          [nil, "register_1", "bit_field_1_0", "[1:0]" , "reserved", nil],
+          [nil, "register_2", "bit_field_2_0", "[3:0]" , "reserved", nil],
+          [nil, "register_3", "bit_field_3_0", "[7:0]" , "reserved", nil],
+          [nil, "register_4", "bit_field_4_0", "[15:0]", "reserved", nil],
+          [nil, "register_5", "bit_field_5_0", "[31:0]", "reserved", nil]
+        ])
+      }.not_to raise_error
+    end
+
+    it "参照ビットフィールドの指定に有無にかかわらず使用できる" do
+      expect {
+        build_bit_fields([
+          [nil, "register_0", "bit_field_0_0", "[0]" , "rw"      , nil            ],
+          [nil, "register_1", "bit_field_1_0", "[0]" , "reserved", nil            ],
+          [nil, "register_2", "bit_field_2_0", "[0]" , "reserved", "bit_field_0_0"]
+        ])
+      }.not_to raise_error
     end
   end
 
-  it "アクセス属性はreserved" do
-    bit_fields  = build_bit_fields([
-      [nil, "register_0", "bit_field_0_0", "[0]", "reserved", nil]
-    ])
-    expect(bit_fields[0]).to match_access(:reserved)
-  end
-
-  it "任意のビット幅を持つビットフィールドで使用できる" do
-    expect {
-      build_bit_fields([
-        [nil, "register_0", "bit_field_0_0", "[0]"   , "reserved", nil],
-        [nil, "register_1", "bit_field_1_0", "[1:0]" , "reserved", nil],
-        [nil, "register_2", "bit_field_2_0", "[3:0]" , "reserved", nil],
-        [nil, "register_3", "bit_field_3_0", "[7:0]" , "reserved", nil],
-        [nil, "register_4", "bit_field_4_0", "[15:0]", "reserved", nil],
-        [nil, "register_5", "bit_field_5_0", "[31:0]", "reserved", nil]
+  describe "ral" do
+    let(:register_map) do
+      set_load_data([
+        [nil, "register_0", "bit_field_0_0", "[0]", "reserved", nil]
       ])
-    }.not_to raise_error
-  end
+      @factory.create(configuration, register_map_file)
+    end
 
-  it "参照ビットフィールドの指定に有無にかかわらず使用できる" do
-    expect {
-      build_bit_fields([
-        [nil, "register_0", "bit_field_0_0", "[0]" , "rw"      , nil            ],
-        [nil, "register_1", "bit_field_1_0", "[0]" , "reserved", nil            ],
-        [nil, "register_2", "bit_field_2_0", "[0]" , "reserved", "bit_field_0_0"]
-      ])
-    }.not_to raise_error
+    let(:ral) do
+      build_ral_factory.create(@configuration, register_map).bit_fields[0]
+    end
+
+    describe "#access" do
+      it "ROを返す" do
+        expect(ral.access).to eq "\"RO\""
+      end
+    end
   end
 end
