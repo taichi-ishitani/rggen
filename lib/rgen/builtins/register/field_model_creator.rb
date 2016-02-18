bc1d350c959713c354e2909317dba32e2fc42266
taichi <taichi730@jf6.so-net.ne.jp>
2016-02-19 00:34:37 +0900

    テンプレート中でコードの生成を行うように変更

--- lib/rgen/builtins/register/field_model_creator.rb
+++ lib/rgen/builtins/register/field_model_creator.rb
@@ -1,12 +1,5 @@
 simple_item :register, :field_model_creator do
   ral do
     generate_code_from_template :reg_model_item
-
-    def function_body(buffer)
-      register.bit_fields.each_with_index do |bit_field, i|
-        buffer << nl if i > 0
-        buffer << bit_field.generate_code(:field_model_creation, :top_down)
-      end
-    end
   end
 end
