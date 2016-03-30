b0ea8fa810a97306e12524525a24fd4bdba633fc
taichi <taichi730@jf6.so-net.ne.jp>
2016-03-30 23:46:43 +0900

    コード生成をメソッド化

--- lib/rgen/builtins/register_block/block_model_definition.rb
+++ lib/rgen/builtins/register_block/block_model_definition.rb
@@ -3,14 +3,16 @@ simple_item :register_block, :block_model_definition do
     generate_code :package_item do
       class_definition model_name do |c|
         c.base :rgen_ral_block
-        c.body do |buffer|
-          register_block.generate_code(:block_model_item, :top_down, buffer)
-        end
+        c.body { |code| body_code(code) }
       end
     end
 
     def model_name
       "#{register_block.name}_block_model"
+    end
+
+    def body_code(code)
+      register_block.generate_code(:block_model_item, :top_down, code)
     end
   end
 end
