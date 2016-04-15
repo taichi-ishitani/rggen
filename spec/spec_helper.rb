8260b50799e69337ef635db392256c9df9c15722
taichi <taichi730@jf6.so-net.ne.jp>
2016-04-16 00:59:50 +0900

    workaround for 'invalid byte sequence in Windows-31J' error

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,9 +3,10 @@ if ENV['TRAVIS']
   CodeClimate::TestReporter.start
 end
 
+Encoding.default_external = Encoding::UTF_8
+
 require_relative '../lib/rggen'
 
 require_relative  'support/helper_methods'
 require_relative  'support/matchers'
 require_relative  'support/config'
-
