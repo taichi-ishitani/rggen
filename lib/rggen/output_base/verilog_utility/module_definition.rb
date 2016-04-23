b7dd9454f223a53ff3a61b0e361660ba4fab1db8
taichi <taichi730@gmail.com>
2016-04-24 01:18:06 +0900

    OutputBase::VerilogUtility::ModuleDefinitionに信号宣言の生成を追加 OutputBase::VerilogUtility::ClassDefinitionにパラメータ宣言と変数宣言の生成を追加

--- lib/rggen/output_base/verilog_utility/module_definition.rb
+++ lib/rggen/output_base/verilog_utility/module_definition.rb
@@ -4,6 +4,12 @@ module RgGen
       class ModuleDefinition < StructureDefinition
         attr_setter :parameters
         attr_setter :ports
+        attr_setter :signals
+
+        def to_code
+          bodies.unshift(signal_declarations) if signals?
+          super
+        end
 
         private
 
@@ -20,17 +26,37 @@ module RgGen
           :endmodule
         end
 
+        def parameters?
+          !(@parameters.nil? || @parameters.empty?)
+        end
+
+        def ports?
+          !(@ports.nil? || @ports.empty?)
+        end
+
+        def signals?
+          !(@signals.nil? || @signals.empty?)
+        end
+
         def parameter_declarations(code)
-          return if @parameters.nil? || @parameters.empty?
-          code << '#('
-          declarations(@parameters, code)
-          code << ')'
+          return unless parameters?
+          wrap(code, '#(', ')') do
+            declarations(@parameters, code)
+          end
         end
 
         def port_declarations(code)
-          code << '('
-          declarations(@ports, code) if @ports && @ports.size > 0
-          code << ')'
+          wrap(code, '(', ')') do
+            declarations(@ports, code) if ports?
+          end
+        end
+
+        def signal_declarations
+          lambda do |code|
+            signals.each do |signal|
+              code << signal << semicolon << nl
+            end
+          end
         end
 
         def declarations(list, code)
