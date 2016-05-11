256656024228f3dd064cf202865d3b7d518d1ec8
taichi <taichi730@gmail.com>
2016-05-12 01:22:39 +0900

    エラーチェックを実装

--- lib/rggen/builtins/register/external.rb
+++ lib/rggen/builtins/register/external.rb
@@ -2,19 +2,25 @@ simple_item :register, :external do
   register_map do
     field :external?
 
-    input_pattern %r{(true)|(false)}i, convert_to_string: true
+    input_pattern %r{(true)|(false)|()}i, convert_to_string: true
 
     build do |cell|
       @external = parse(cell)
       register.need_no_children if external?
     end
 
+    validate do
+      if (register.array? || register.shadow?) && external?
+        error 'not use array/shadow and ' \
+              'external register on the same register'
+      end
+    end
+
     def parse(cell)
-      case
-      when pattern_matched?
+      if pattern_matched?
         captures.first.not_nil?
-      when cell.nil? || cell.empty?
-        false
+      else
+        error "invalid value for 'external': #{cell.inspect}"
       end
     end
   end
--- spec/rggen/builtins/register/external_spec.rb
+++ spec/rggen/builtins/register/external_spec.rb
@@ -30,33 +30,40 @@ describe 'register/external' do
   end
 
   let(:registers) do
+    set_load_data(load_data)
+    @factory.create(configuration, register_map_file).registers
+  end
+
+  def set_load_data(data)
     block_data  = [
       [nil, nil, "block_0"],
       [nil, nil, 256      ],
       [                   ],
       [                   ]
     ]
-    block_data.concat(load_data)
+    block_data.concat(data)
     RegisterMapDummyLoader.load_data("block_0" => block_data)
-    @factory.create(configuration, register_map_file).registers
   end
 
   context "入力がnilや空文字の場合" do
     let(:load_data) do
       [
         [nil, "register_0", "0x00", nil, nil, nil, "bit_field_0_0", "[0]", "rw", 0],
-        [nil, "register_1", "0x04", nil, nil, "" , "bit_field_1_0", "[0]", "rw", 0]
+        [nil, "register_1", "0x04", nil, nil, "" , "bit_field_1_0", "[0]", "rw", 0],
+        [nil, "register_2", "0x08", nil, nil, " ", "bit_field_2_0", "[0]", "rw", 0]
       ]
     end
 
     specify "当該レジスタは内部レジスタである" do
       expect(registers[0]).not_to be_external
       expect(registers[1]).not_to be_external
+      expect(registers[2]).not_to be_external
     end
 
     specify "当該レジスタはビットフィールドを持つ" do
       expect(registers[0].bit_fields).not_to be_empty
       expect(registers[1].bit_fields).not_to be_empty
+      expect(registers[2].bit_fields).not_to be_empty
     end
   end
 
@@ -107,6 +114,58 @@ describe 'register/external' do
       expect(registers[1].bit_fields).to be_empty
       expect(registers[2].bit_fields).to be_empty
       expect(registers[3].bit_fields).to be_empty
+    end
+  end
+
+  context "入力がtrue, false, nil, 空白でない場合" do
+    let(:invalid_values) do
+      [1, 0, :foo, "BAR", "NIL"]
+    end
+
+    it "RegisterMapErrorを発生させる" do
+      invalid_values.each do |invalid_value|
+        set_load_data([
+          [nil, "register_0", "0x00", nil, nil, invalid_value]
+        ])
+        expect {
+          @factory.create(configuration, register_map_file)
+        }.to raise_register_map_error("invalid value for 'external': #{invalid_value.inspect}", position("block_0", 4, 5))
+      end
+    end
+  end
+
+  context "配列レジスタかつ外部レジスタの場合" do
+    it "RegisterMapErrorを発生させる" do
+      set_load_data([
+        [nil, "register_0", "0x00-0x07", "[2]", nil, true]
+      ])
+      expect {
+        @factory.create(configuration, register_map_file)
+      }.to raise_register_map_error("not use array/shadow and external register on the same register", position("block_0", 4, 5))
+    end
+  end
+
+  context "シャドウレジスタかつ外部レジスタの場合" do
+    it "RegisterMapErrorを発生させる" do
+      set_load_data([
+        [nil, "register_0", "0x00", nil, "bit_field_1_0:1", true                                ],
+        [nil, "register_1", "0x04", nil, nil              , nil, "bit_field_1_0", "[0]", "rw", 0]
+      ])
+      expect {
+        @factory.create(configuration, register_map_file)
+      }.to raise_register_map_error("not use array/shadow and external register on the same register", position("block_0", 4, 5))
+    end
+  end
+
+  context "配列レジスタ、または、シャドウレジスタかつ内部レジスタの場合" do
+    it "エラーなく使用できる" do
+      set_load_data([
+        [nil, "register_0", "0x00", "[2]", "bit_field_1_0", nil, "bit_field_0_0", "[0]", "rw", 0],
+        [nil, "register_1", "0x04", nil  , nil            , nil, "bit_field_1_0", "[0]", "rw", 0]
+      ])
+      expect {
+        @factory.create(configuration, register_map_file)
+      }.not_to raise_error
     end
   end
 end
