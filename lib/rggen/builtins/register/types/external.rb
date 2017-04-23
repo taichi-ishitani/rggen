b7258a44b75a729709c12cabb19b3636816cbaa3
taichi <taichi730@gmail.com>
2017-04-24 02:03:01 +0900

    register/types/externalにアドレス構造体メンバー定義出力を実装(#4)

--- lib/rggen/builtins/register/types/external.rb
+++ lib/rggen/builtins/register/types/external.rb
@@ -4,4 +4,12 @@ list_item :register, :type, :external do
     required_byte_size any_size
     need_no_bit_fields
   end
+
+  c_header do
+    delegate [:name, :byte_size] => :register
+
+    address_struct_member do
+      "RGGEN_EXTERNAL_REGISTERS(#{byte_size}, #{name.upcase}) #{name};"
+    end
+  end
 end
