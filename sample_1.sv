dbcd6303c65f19baaa02ca4b7d8b5bf9fe8f2318
taichi <taichi730@gmail.com>
2017-05-16 15:48:49 +0900

    Squashed 'sample/' changes from b0c5789..5eb47cb
    5eb47cb fixed MDAI error reported by VCS
    168e2d4 更新
    
    git-subtree-dir: sample
    git-subtree-split: 5eb47cb3262700634e45d75d090159734771de13

--- sample_1.sv
+++ sample_1.sv
@@ -9,9 +9,6 @@ module sample_1 (
   output o_bit_field_2_1
 );
   rggen_register_if #(7, 32) register_if[3]();
-  rggen_bit_field_if #(32) register_0_bit_field_if[2]();
-  rggen_bit_field_if #(32) register_1_bit_field_if[1]();
-  rggen_bit_field_if #(32) register_2_bit_field_if[2]();
   rggen_host_if_apb #(
     .LOCAL_ADDRESS_WIDTH  (7),
     .DATA_WIDTH           (32),
@@ -22,79 +19,88 @@ module sample_1 (
     .apb_if       (apb_if),
     .register_if  (register_if)
   );
-  rggen_default_register #(
-    .ADDRESS_WIDTH    (7),
-    .START_ADDRESS    (7'h00),
-    .END_ADDRESS      (7'h03),
-    .DATA_WIDTH       (32),
-    .TOTAL_BIT_FIELDS (2),
-    .MSB_LIST         ('{31, 15}),
-    .LSB_LIST         ('{16, 0})
-  ) u_register_0 (
-    .register_if  (register_if[0]),
-    .bit_field_if (register_0_bit_field_if)
-  );
-  rggen_bit_field_rw #(
-    .WIDTH          (16),
-    .INITIAL_VALUE  (16'h0000)
-  ) u_bit_field_0_0 (
-    .clk          (clk),
-    .rst_n        (rst_n),
-    .bit_field_if (register_0_bit_field_if[0]),
-    .o_value      (o_bit_field_0_0)
-  );
-  rggen_bit_field_ro #(
-    .WIDTH  (16)
-  ) u_bit_field_0_1 (
-    .bit_field_if (register_0_bit_field_if[1]),
-    .i_value      (i_bit_field_0_1)
-  );
-  rggen_default_register #(
-    .ADDRESS_WIDTH    (7),
-    .START_ADDRESS    (7'h04),
-    .END_ADDRESS      (7'h07),
-    .DATA_WIDTH       (32),
-    .TOTAL_BIT_FIELDS (1),
-    .MSB_LIST         ('{31}),
-    .LSB_LIST         ('{0})
-  ) u_register_1 (
-    .register_if  (register_if[1]),
-    .bit_field_if (register_1_bit_field_if)
-  );
-  rggen_bit_field_rw #(
-    .WIDTH          (32),
-    .INITIAL_VALUE  (32'h00000000)
-  ) u_bit_field_1_0 (
-    .clk          (clk),
-    .rst_n        (rst_n),
-    .bit_field_if (register_1_bit_field_if[0]),
-    .o_value      (o_bit_field_1_0)
-  );
-  rggen_default_register #(
-    .ADDRESS_WIDTH    (7),
-    .START_ADDRESS    (7'h08),
-    .END_ADDRESS      (7'h0b),
-    .DATA_WIDTH       (32),
-    .TOTAL_BIT_FIELDS (2),
-    .MSB_LIST         ('{16, 0}),
-    .LSB_LIST         ('{16, 0})
-  ) u_register_2 (
-    .register_if  (register_if[2]),
-    .bit_field_if (register_2_bit_field_if)
-  );
-  rggen_bit_field_ro #(
-    .WIDTH  (1)
-  ) u_bit_field_2_0 (
-    .bit_field_if (register_2_bit_field_if[0]),
-    .i_value      (i_bit_field_2_0)
-  );
-  rggen_bit_field_rw #(
-    .WIDTH          (1),
-    .INITIAL_VALUE  (1'h0)
-  ) u_bit_field_2_1 (
-    .clk          (clk),
-    .rst_n        (rst_n),
-    .bit_field_if (register_2_bit_field_if[1]),
-    .o_value      (o_bit_field_2_1)
-  );
+  generate if (1) begin : g_register_0
+    rggen_bit_field_if #(32) bit_field_if[2]();
+    rggen_default_register #(
+      .ADDRESS_WIDTH    (7),
+      .START_ADDRESS    (7'h00),
+      .END_ADDRESS      (7'h03),
+      .DATA_WIDTH       (32),
+      .TOTAL_BIT_FIELDS (2),
+      .MSB_LIST         ('{31, 15}),
+      .LSB_LIST         ('{16, 0})
+    ) u_register_0 (
+      .register_if  (register_if[0]),
+      .bit_field_if (bit_field_if)
+    );
+    rggen_bit_field_rw #(
+      .WIDTH          (16),
+      .INITIAL_VALUE  (16'h0000)
+    ) u_bit_field_0_0 (
+      .clk          (clk),
+      .rst_n        (rst_n),
+      .bit_field_if (bit_field_if[0]),
+      .o_value      (o_bit_field_0_0)
+    );
+    rggen_bit_field_ro #(
+      .WIDTH  (16)
+    ) u_bit_field_0_1 (
+      .bit_field_if (bit_field_if[1]),
+      .i_value      (i_bit_field_0_1)
+    );
+  end endgenerate
+  generate if (1) begin : g_register_1
+    rggen_bit_field_if #(32) bit_field_if[1]();
+    rggen_default_register #(
+      .ADDRESS_WIDTH    (7),
+      .START_ADDRESS    (7'h04),
+      .END_ADDRESS      (7'h07),
+      .DATA_WIDTH       (32),
+      .TOTAL_BIT_FIELDS (1),
+      .MSB_LIST         ('{31}),
+      .LSB_LIST         ('{0})
+    ) u_register_1 (
+      .register_if  (register_if[1]),
+      .bit_field_if (bit_field_if)
+    );
+    rggen_bit_field_rw #(
+      .WIDTH          (32),
+      .INITIAL_VALUE  (32'h00000000)
+    ) u_bit_field_1_0 (
+      .clk          (clk),
+      .rst_n        (rst_n),
+      .bit_field_if (bit_field_if[0]),
+      .o_value      (o_bit_field_1_0)
+    );
+  end endgenerate
+  generate if (1) begin : g_register_2
+    rggen_bit_field_if #(32) bit_field_if[2]();
+    rggen_default_register #(
+      .ADDRESS_WIDTH    (7),
+      .START_ADDRESS    (7'h08),
+      .END_ADDRESS      (7'h0b),
+      .DATA_WIDTH       (32),
+      .TOTAL_BIT_FIELDS (2),
+      .MSB_LIST         ('{16, 0}),
+      .LSB_LIST         ('{16, 0})
+    ) u_register_2 (
+      .register_if  (register_if[2]),
+      .bit_field_if (bit_field_if)
+    );
+    rggen_bit_field_ro #(
+      .WIDTH  (1)
+    ) u_bit_field_2_0 (
+      .bit_field_if (bit_field_if[0]),
+      .i_value      (i_bit_field_2_0)
+    );
+    rggen_bit_field_rw #(
+      .WIDTH          (1),
+      .INITIAL_VALUE  (1'h0)
+    ) u_bit_field_2_1 (
+      .clk          (clk),
+      .rst_n        (rst_n),
+      .bit_field_if (bit_field_if[1]),
+      .o_value      (o_bit_field_2_1)
+    );
+  end endgenerate
 endmodule
