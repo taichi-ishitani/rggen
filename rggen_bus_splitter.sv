fd6d38b4a6e5fb6d5f075ac7d22bef8c93988f33
taichi <taichi730@gmail.com>
2017-05-31 19:14:35 +0900

    Squashed 'rtl/' changes from 4be8174..1b37ebc
    1b37ebc Merge pull request #6 from taichi-ishitani/bugfix/5
    33a28e8 fixed invalid logic reaported by SpyGlass lint
    24d896e fix #5
    
    git-subtree-dir: rtl
    git-subtree-split: 1b37ebc12155c8147bfb98712e9a56e681984e8c

--- rggen_bus_splitter.sv
+++ rggen_bus_splitter.sv
@@ -9,29 +9,16 @@ module rggen_bus_splitter #(
 );
   import  rggen_rtl_pkg::*;
 
-  localparam  INDEX_WIDTH   = $clog2(TOTAL_REGISTERS+1);
+  localparam  int STATUS_WIDTH  = $bits(rggen_status);
 
-  typedef struct packed {
-    logic [DATA_WIDTH-1:0]  read_data;
-    rggen_status            status;
-  } s_response;
-
-  logic [TOTAL_REGISTERS:0]   select;
-  logic [TOTAL_REGISTERS:0]   ready;
-  s_response                  response[TOTAL_REGISTERS+1];
+  logic [TOTAL_REGISTERS-1:0] select;
+  logic [TOTAL_REGISTERS-1:0] ready;
   logic                       response_ready;
-  logic                       no_register_selected;
+  logic                       register_selected;
   logic                       done;
-  logic                       read_done;
-  logic                       write_done;
-  s_response                  selected_response;
-  genvar                      g_i;
-
-  assign  bus_if.done       = done;
-  assign  bus_if.read_done  = read_done;
-  assign  bus_if.write_done = write_done;
-  assign  bus_if.read_data  = selected_response.read_data;
-  assign  bus_if.status     = selected_response.status;
+  logic [DATA_WIDTH-1:0]      selected_read_data;
+  rggen_status                selected_status;
+  genvar                      g_i, g_j;
 
   generate for (g_i = 0;g_i < TOTAL_REGISTERS;++g_i) begin : g
     assign  register_if[g_i].request      = bus_if.request;
@@ -41,47 +28,59 @@ module rggen_bus_splitter #(
     assign  register_if[g_i].write_strobe = bus_if.write_strobe;
     assign  select[g_i]                   = register_if[g_i].select;
     assign  ready[g_i]                    = register_if[g_i].ready;
-    assign  response[g_i].read_data       = register_if[g_i].read_data;
-    assign  response[g_i].status          = register_if[g_i].status;
   end endgenerate
 
-  //  dummy response
-  assign  no_register_selected      = ~|select[TOTAL_REGISTERS-1:0];
-  assign  select[TOTAL_REGISTERS]   = no_register_selected;
-  assign  ready[TOTAL_REGISTERS]    = no_register_selected;
-  assign  response[TOTAL_REGISTERS] = '{read_data: '0, status: RGGEN_SLAVE_ERROR};
-
-  assign  response_ready  = |ready;
+  assign  bus_if.done       = done;
+  assign  response_ready    = |ready;
+  assign  register_selected = |select;
   always_ff @(posedge clk, negedge rst_n) begin
     if (!rst_n) begin
       done              <= '0;
-      read_done         <= '0;
-      write_done        <= '0;
-      selected_response <= '{read_data: '0, status: RGGEN_OKAY};
+      bus_if.read_done  <= '0;
+      bus_if.write_done <= '0;
+      bus_if.read_data  <= '0;
+      bus_if.status     <= RGGEN_OKAY;
     end
-    else if (bus_if.request && response_ready && (!done)) begin
+    else if (bus_if.request && (response_ready || (!register_selected)) && (!done)) begin
       done              <= '1;
-      write_done        <= (bus_if.direction == RGGEN_WRITE) ? '1 : '0;
-      read_done         <= (bus_if.direction == RGGEN_READ ) ? '1 : '0;
-      selected_response <= response[calc_index()];
+      bus_if.read_done  <= (bus_if.direction == RGGEN_READ ) ? '1 : '0;
+      bus_if.write_done <= (bus_if.direction == RGGEN_WRITE) ? '1 : '0;
+      if (register_selected) begin
+        bus_if.read_data  <= selected_read_data;
+        bus_if.status     <= selected_status;
+      end
+      else begin
+        bus_if.read_data  <= '0;
+        bus_if.status     <= RGGEN_SLAVE_ERROR;
+      end
     end
     else begin
       done              <= '0;
-      read_done         <= '0;
-      write_done        <= '0;
-      selected_response <= '{read_data: '0, status: RGGEN_OKAY};
+      bus_if.read_done  <= '0;
+      bus_if.write_done <= '0;
+      bus_if.read_data  <= '0;
+      bus_if.status     <= RGGEN_OKAY;
     end
   end
 
-  function automatic logic [INDEX_WIDTH-1:0] calc_index();
-    logic [INDEX_WIDTH-1:0] index;
-    for (int i = 0;i < INDEX_WIDTH;++i) begin
-      logic [TOTAL_REGISTERS:0] temp;
-      for (int j = 0;j <= TOTAL_REGISTERS;++j) begin
-        temp[j] = j[i] & select[j];
+  //  Response Selection
+  generate if (1) begin : read_data_selection
+    for (g_i = 0;g_i < DATA_WIDTH;++g_i) begin : g
+      logic [TOTAL_REGISTERS-1:0] temp;
+      assign  selected_read_data[g_i] = |temp;
+      for (g_j = 0;g_j < TOTAL_REGISTERS;++g_j) begin : g
+        assign  temp[g_j] = register_if[g_j].read_data[g_i] & register_if[g_j].select;
       end
-      index[i]  = |temp;
     end
-    return index;
-  endfunction
+  end endgenerate
+
+  generate if (1) begin : status_selection
+    for (g_i = 0;g_i < STATUS_WIDTH;++g_i) begin : g
+      logic [TOTAL_REGISTERS-1:0] temp;
+      assign  selected_status[g_i]  = |temp;
+      for (g_j = 0;g_j < TOTAL_REGISTERS;++g_j) begin : g
+        assign  temp[g_j] = register_if[g_j].status[g_i] & register_if[g_j].select;
+      end
+    end
+  end endgenerate
 endmodule
