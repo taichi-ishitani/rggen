3c9a4ad3d659cf9c7688e83e739871328fbab817
taichi <taichi730@gmail.com>
2019-05-15 18:41:50 +0900

    fix #132

--- lib/rggen/builtins/bit_field/reference.rb
+++ lib/rggen/builtins/bit_field/reference.rb
@@ -2,7 +2,7 @@ simple_item :bit_field, :reference do
   register_map do
     field :reference, need_validation: true, forward_to: :find_reference
     field :has_reference? do
-      @reference.not.empty?
+      !(@reference.nil? || @reference.empty?)
     end
 
     build do |cell|
